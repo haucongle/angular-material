cd00ee047ddb8c4c543916f8b2ab3fad5b2c0df0
Hau Le <haule@hoiio.com>
2018-09-18 18:34:40 +0700

    add components

---
+++ src/app/my-nav/my-nav.component.spec.ts
@@ -0,0 +1,24 @@
+import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MyNavComponent} from './my-nav.component';
+
+describe('MyNavComponent', () => {
+  let component: MyNavComponent;
+  let fixture: ComponentFixture<MyNavComponent>;
+
+  beforeEach(fakeAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [MatSidenavModule],
+      declarations: [MyNavComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MyNavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
