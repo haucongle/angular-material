45f26724f0d5d5cc9be0d40263c14072765eecf5
Hau Le <leconghau.me@gmail.com>
2018-09-19 15:00:58 +0700

    complete blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,43 +3,37 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {LayoutModule} from '@angular/cdk/layout';
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatGridListModule,
-  MatIconModule,
-  MatListModule,
-  MatMenuModule,
-  MatPaginatorModule,
-  MatSidenavModule,
-  MatSortModule,
-  MatTableModule,
-  MatToolbarModule
-} from '@angular/material';
+import {MaterialModule} from './material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AppRouters} from './app.routes';
+import {DataService} from './data/data.service';
+import {AuthService} from './auth.service';
+import {PostDialogComponent} from './post-dialog/post-dialog.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    DashboardComponent,
+    PostDialogComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule
+    MaterialModule,
+    FlexLayoutModule,
+    AppRouters,
+    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    DataService,
+    AuthService,
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [PostDialogComponent],
 })
 export class AppModule {}
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,57 @@
+import {Component} from '@angular/core';
+import {DataService} from '../data/data.service';
+import {Post} from '../post';
+import {Observable} from 'rxjs';
+import {DataSource} from '@angular/cdk/table';
+import {AuthService} from '../auth.service';
+import {PostDialogComponent} from '../post-dialog/post-dialog.component';
+import {MatDialog} from '@angular/material';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+  displayedColumns = ['date_posted', 'title', 'category', 'delete'];
+  dataSource = new PostDataSource(this.dataService);
+
+  constructor(
+    private dataService: DataService,
+    public auth: AuthService,
+    public dialog: MatDialog
+  ) { }
+
+  deletePost(id) {
+    if (this.auth.isAuthenticated()) {
+      this.dataService.deletePost(id);
+      this.dataSource = new PostDataSource(this.dataService);
+    } else {
+      alert('Login in Before');
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(PostDialogComponent, {
+      width: '600px',
+      data: 'Add Post'
+    });
+    dialogRef.componentInstance.event.subscribe((result) => {
+      this.dataService.addPost(result.data);
+      this.dataSource = new PostDataSource(this.dataService);
+    });
+  }
+}
+
+export class PostDataSource extends DataSource<any> {
+  constructor(private dataService: DataService) {
+    super();
+  }
+
+  connect(): Observable<Post[]> {
+    return this.dataService.getData();
+  }
+
+  disconnect() { }
+}
