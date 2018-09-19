c0474a858aaccd4c706a958e012b9338db822f05
Hau Le <leconghau.me@gmail.com>
2018-09-19 15:40:42 +0700

    cleanup2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,14 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
-import {WelcomeComponent} from './welcome/welcome.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
 import {AppRouters} from './app.routes';
-import {DataService} from './data/data.service';
 import {AuthService} from './auth.service';
-import {PostDialogComponent} from './post-dialog/post-dialog.component';
 import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    DashboardComponent,
-    PostDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,10 +23,8 @@ import {FormsModule} from '@angular/forms';
     FormsModule,
   ],
   providers: [
-    DataService,
     AuthService,
   ],
   bootstrap: [AppComponent],
-  entryComponents: [PostDialogComponent],
 })
 export class AppModule {}
