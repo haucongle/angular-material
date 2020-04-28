055c5d3dd6c84aabf58466e8bd3faf9b9e9ad709
Hau Le <haule@hoiio.com>
2020-04-28 13:22:44 +0700

    init apps and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppComponent} from './app.component';
-import {DemoMaterialModule} from './shared/material/material.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { DemoMaterialModule } from './shared/material/material.module';
+import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WhatsappComponent } from './apps/whatsapp/whatsapp.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LeftSidebarComponent,
+    WhatsappComponent,
+  ],
   imports: [BrowserModule, BrowserAnimationsModule, DemoMaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
