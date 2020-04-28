e6ae740caa7c1dca5897f94f0bd33bfdeba30050
Hau Le <haule@hoiio.com>
2020-04-28 13:45:34 +0700

    restructor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { DemoMaterialModule } from './shared/material/material.module';
 import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './apps/whatsapp/header/header.component';
 import { WhatsappComponent } from './apps/whatsapp/whatsapp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     LeftSidebarComponent,
     WhatsappComponent,
+    HeaderComponent
   ],
   imports: [BrowserModule, BrowserAnimationsModule, DemoMaterialModule],
   providers: [],
