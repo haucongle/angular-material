4183b44bf3eea3a6d652a9a56a2e2c5faa97adc7
Hau Le <leconghau.me@gmail.com>
2020-04-29 23:11:57 +0700

    add background color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AvatarModule } from 'ngx-avatar';
 import { AppComponent } from './app.component';
-import { DemoMaterialModule } from './shared/material/material.module';
-import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
 import { HeaderComponent } from './apps/whatsapp/header/header.component';
 import { WhatsappComponent } from './apps/whatsapp/whatsapp.component';
+import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
+import { DemoMaterialModule } from './shared/material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LeftSidebarComponent,
     WhatsappComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
-  imports: [BrowserModule, BrowserAnimationsModule, DemoMaterialModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    AvatarModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
